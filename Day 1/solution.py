488f66544e16f01ee9069a0e0d60d0e9a7c7b22e
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2023-12-01 19:13:08 +0000

    Update solution for part 2

---
+++ Day 1/solution.py
@@ -0,0 +1,31 @@
+import re
+
+
+def sum_first_last_digit(file_path):
+    total_sum = 0
+    # Dictionary mapping number words to their digit equivalents
+    numbers = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+
+    # Open the file and read it line by line
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Finds all the numbers
+            nums = re.findall(r'(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))', line)
+            first = numbers.get(nums[0], nums[0])
+            last = numbers.get(nums[-1], nums[-1])
+            total_sum += int(first + last)
+
+    return total_sum
+
+# Example usage
+print(sum_first_last_digit("input.txt"))
