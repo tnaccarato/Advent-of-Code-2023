4cb23eef92b88d2a29ea0f83bfb9d2fab702b587
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2023-12-06 12:01:51 +0000

    Add part 2 solution

--- Day 6/solution.py
+++ Day 6/solution.py
@@ -5,33 +5,23 @@ Distance:  9  40  200"""
 
 
 def ways_to_beat_record(string):
-    # Declares a variable for storing the product of all the permutations
-    total_ways = 1
-
     # Splits the string into time and distances
-    times, record_distances = string.split("\n")
+    time, record_distance = string.split("\n")
     # Defines a regex pattern for matching the numbers
     pattern = r'\d+'
     # Extracts numbers from each line and casts them to int
-    times = [int(number) for number in re.findall(pattern, times)]
-    record_distances = [int(number) for number in re.findall(pattern, record_distances)]
+    time = int("".join([number for number in re.findall(pattern, time)]))
+    record_distance = int("".join([number for number in re.findall(pattern, record_distance)]))
 
-    # Maps each race time to a record distance
-    races = list(zip(times, record_distances))
+    beat_record_time = 0  # Initialises counter for ways to beat record
+    for hold_duration in range(time):  # Iterate through possible hold durations
+        boat_speed = hold_duration
+        travel_time = time - hold_duration
+        distance_traveled = boat_speed * travel_time
+        if distance_traveled > record_distance:  # Check if the boat beats the record
+            beat_record_time += 1
 
-    for race in races:
-        beat_record_time = 0  # Initialises counter for ways to beat record
-        time = race[0]
-        record = race[1]
-        for hold_duration in range(time):  # Iterate through possible hold durations
-            boat_speed = hold_duration
-            travel_time = time - hold_duration
-            distance_traveled = boat_speed * travel_time
-            if distance_traveled > record:  # Check if the boat beats the record
-                beat_record_time += 1
-        total_ways *= beat_record_time  # Multiply the ways for this race with the total
-
-    return total_ways
+    return beat_record_time
 
 
 with open("input.txt", "r") as input_string:
